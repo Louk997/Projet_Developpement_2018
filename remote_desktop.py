e67c60647b001f37ae9dc4c53e30206d945d29a0
Louk997 <37746159+Louk997@users.noreply.github.com>
2018-12-20 21:23:47 +0100

    Update remote_desktop.py

--- remote_desktop.py
+++ remote_desktop.py
@@ -1,29 +1,38 @@
 from Class_Projet import Client
 import argparse
+import time
 
 client = Client()
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog="Remote Desktop",
+                                 description="Optional settings for a quick use of the program",
+                                 prefix_chars="/-",
+                                 add_help=False,
+                                 conflict_handler="resolve")
 
-parser.add_argument("-s", "--shell", action="store_true", help="<<Access directly to the remote shell>>")
-parser.add_argument("-i", "--informations", choices=["menu", "computername", "network", "currentuser", "allusers"],
+parser.add_argument("/h", "--help", action="help", help="<<Show all the existing options>>")
+parser.add_argument("/s", "--shell", action="store_true", help="<<Access directly to the remote shell>>")
+parser.add_argument("/m", "--infomenu", action="store_true", help="<<Access directly to the information menu>>")
+parser.add_argument("/i", "--informations", choices=["computername", "network", "currentuser", "allusers"],
                     help="<<Directly get informations>>")
-parser.add_argument("-b", "--buffsize", type=int, choices=[1024, 2048, 4096, 8192, 16384], default=4096,
+parser.add_argument("/b", "/buffsize", type=int, choices=[1024, 2048, 4096, 8192, 16384], default=4096,
                     help="<<Choose an optional buffsize (default = 4096),>>")
 
 args = parser.parse_args()
+
 
 client.start()
 client.conn()
 
 if args.shell:
     choice = "1"
-elif args.informations:
+elif args.informations or args.infomenu:
     choice = "2"
 else:
     choice = client.main_menu()
 
+client.send(choice)
+time.sleep(2)
 while choice != "0" and choice is not None:
-    client.send(choice)
     if choice == "1":
         while choice != "4":
             print(client.receive(), end="")
@@ -38,10 +47,19 @@ while choice != "0" and choice is not None:
 
     elif choice == "2":
         if args.informations == "computername":
-            choice2 = "1"
+            client.info_arg("computer")
+        if args.informations == "network":
+            client.info_arg("network")
+        if args.informations == "currentuser":
+            client.info_arg("current")
+        if args.informations == "allusers":
+            client.info_arg("users")
         else:
             choice2 = client.display_choice()
 
         client.choice_information(choice2)
 
     choice = client.main_menu()
+    client.send(choice)
+
+client.quit()
