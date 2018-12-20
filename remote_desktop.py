2bcea57b459a4499bb4f6d29ba3012ce1bc1ac0e
Louk997 <37746159+Louk997@users.noreply.github.com>
2018-12-20 21:19:20 +0100

    Update remote_desktop.py

--- remote_desktop.py
+++ remote_desktop.py
@@ -11,7 +11,20 @@ if choix == "1":
     while True:
         cmd = input()
         client.send(cmd)
-        if cmd == "quit()":
+        if cmd == "quit":
             client.quit()
         else:
             client.receive()
+elif choix == "2":
+    while True:
+        choix2 = client.choice_information()
+        if choix2 == "1":
+            client.send("computer")
+        if choix2 == "2":
+            client.send("computer")
+        if choix2 == "3":
+            client.send("computer")
+        if choix2 == "4":
+            client.send("computer")
+        if choix2 == "5":
+            client.send("computer")
